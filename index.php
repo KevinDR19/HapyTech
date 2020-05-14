b24d26a27cd8c363e999a8208120fe8235aaf28c
KevinDR19 <kevin26rg@hotmail.com>
2020-05-14 13:22:47 -0500

    Agregamos el favicon

--- index.php
+++ index.php
@@ -3,7 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Registro de usuarios</title>
+
+    <link rel="shortcut icon" href="Publicos/Imagenes/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="Publicos/Imagenes/favicon.ico" type="image/x-icon">
+
 </head>
 <body>
     
