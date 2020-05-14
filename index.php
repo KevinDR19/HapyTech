a922b42e9f42f3d923b5557741efe17995e40f8b
KevinDR19 <kevin26rg@hotmail.com>
2020-05-14 13:41:16 -0500

    Hacemos referencia a librerias css

--- index.php
+++ index.php
@@ -8,6 +8,11 @@
     <link rel="shortcut icon" href="Publicos/Imagenes/favicon.ico" type="image/x-icon">
     <link rel="icon" href="Publicos/Imagenes/favicon.ico" type="image/x-icon">
 
+    <!--Css-->
+    <link rel="stylesheet" href="Librerias/bootstrap-4.4.1/css/bootstrap.css">
+    <link rel="stylesheet" href="Librerias/fontawesome-5.13.0/css/all.css">
+    <link rel="stylesheet" href="Librerias/sweetalert2/sweetalert/sweetalert2.min.css">
+
 </head>
 <body>
     
